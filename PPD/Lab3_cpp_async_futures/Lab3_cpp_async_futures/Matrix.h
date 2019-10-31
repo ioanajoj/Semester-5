3c714dbb73dbb360e8e0c8b8b616bf19f9e6c28e
Ioana Joj <ioanna.gheorghiu@gmail.com>
2019-10-31 17:38:52 +0200

    C++ Async and Futures

---
+++ PPD/Lab3_cpp_async_futures/Lab3_cpp_async_futures/Matrix.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+
+class Matrix
+{
+public:
+	int** matrix;
+	unsigned rows, columns;
+
+	Matrix();
+	/*Matrix(const Matrix &another_matrix);*/
+	Matrix(unsigned rows, unsigned columns, int matrix[100][100]);
+	Matrix(unsigned rows, unsigned columns, int** matrix);
+
+	friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix);
+	friend Matrix operator + (Matrix const & matrix1, Matrix const & matrix2);
+	friend Matrix operator * (Matrix const & matrix1, Matrix const & matrix2);
+
+	~Matrix();
+};
+
+#pragma once
