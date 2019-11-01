0b4043866d1f91884d3c90e2621786bda3e89075
Ioana Joj <ioanna.gheorghiu@gmail.com>
2019-11-01 11:39:48 +0200

    documentation

---
+++ PPD/Lab3_java_tasks_futures/src/domain/SumTask.java
@@ -0,0 +1,30 @@
+package domain;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @author joj on 11/1/2019
+ **/
+public class SumTask implements Callable<Integer> {
+    Matrix sum_matrix, matrix1, matrix2;
+    int cell;
+
+    public SumTask(Matrix sum_matrix, Matrix matrix1, Matrix matrix2, int cell) {
+        this.sum_matrix = sum_matrix;
+        this.matrix1 = matrix1;
+        this.matrix2 = matrix2;
+        this.cell = cell;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+//        System.out.println("Adding cell " + cell);
+        int row = cell / matrix1.columns;
+        int column = cell % matrix1.columns;
+        int sum = 0;
+        if (row < matrix1.rows && column < matrix1.columns) sum += matrix1.matrix[row][column];
+        if (row < matrix2.rows && column < matrix2.columns) sum += matrix2.matrix[row][column];
+        sum_matrix.matrix[row][column] = sum;
+        return sum;
+    }
+}
