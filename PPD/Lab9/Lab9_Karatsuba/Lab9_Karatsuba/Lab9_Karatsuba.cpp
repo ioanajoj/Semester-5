c8f92086606df9b0fb99ff8ec6e1f979cb52dde1
Ioana Joj <ioanna.gheorghiu@gmail.com>
2020-02-05 22:52:18 +0200

    Lab9 MPI Broadcast

--- PPD/Lab9/Lab9_Karatsuba/Lab9_Karatsuba/Lab9_Karatsuba.cpp
+++ PPD/Lab9/Lab9_Karatsuba/Lab9_Karatsuba/Lab9_Karatsuba.cpp
@@ -61,6 +61,8 @@ void multiply_sequncial(long *A, int m, long *B, int n)
 	print_polynomial("Result seq: ", C, size_C);
 }
 
+const bool BROADCAST = true;
+
 int main(int argc, char* argv[])
 {
 	int pid,	// process id
@@ -81,26 +83,36 @@ int main(int argc, char* argv[])
 		printf("Karatsuba Polynomial Multiplication!\n");
 		printf("Running on %d processes\n", np);
 
-		int size_P = 100000, size_Q = 100000;
+		int size_P = 10000, size_Q = 10000;
 		long *P = get_rand_polynomial(size_P);
 		long *Q = get_rand_polynomial(size_Q);
 
-		//		print_polynomial("P", P, size_P);
-		//		print_polynomial("Q", Q, size_Q);
+		//print_polynomial("P", P, size_P);
+		//print_polynomial("Q", Q, size_Q);
 
 		auto t1 = std::chrono::high_resolution_clock::now();
 
-		for (int i = 1; i < np; i++)
-		{
-			// send sizes
-			int metadata[2];
-			metadata[0] = size_P;
-			metadata[1] = size_Q;
-			MPI_Ssend(metadata, 2, MPI_INT, i, 0, MPI_COMM_WORLD);
+		int metadata[2];
+		metadata[0] = size_P;
+		metadata[1] = size_Q;
 
-			// send polynomials
-			MPI_Ssend(P, size_P, MPI_LONG, i, 1, MPI_COMM_WORLD);
-			MPI_Ssend(Q, size_Q, MPI_LONG, i, 2, MPI_COMM_WORLD);
+		if (BROADCAST)
+		{
+			MPI_Bcast(metadata, 2, MPI_INT, 0, MPI_COMM_WORLD);
+			MPI_Bcast(P, size_P, MPI_LONG, 0, MPI_COMM_WORLD);
+			MPI_Bcast(Q, size_Q, MPI_LONG, 0, MPI_COMM_WORLD);
+		}
+		else
+		{
+			for (int i = 1; i < np; i++)
+			{
+				// send sizes
+				MPI_Ssend(metadata, 2, MPI_INT, i, 0, MPI_COMM_WORLD);
+
+				// send polynomials
+				MPI_Ssend(P, size_P, MPI_LONG, i, 1, MPI_COMM_WORLD);
+				MPI_Ssend(Q, size_Q, MPI_LONG, i, 2, MPI_COMM_WORLD);
+			}
 		}
 
 		long *Res = karatsuba_worker(pid, P, size_P, Q, size_Q, np);
@@ -118,27 +130,41 @@ int main(int argc, char* argv[])
 		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
 		std::cout << "exec time: " << duration << std::endl;
 
-		//		print_polynomial("Result MPI: ", Res, size_Res);
+		//print_polynomial("Result MPI: ", Res, size_Res);
 	}
 	else
 	{
 		// receive metadata
 		int metadata[2];
-		MPI_Recv(metadata, 2, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-		int sender = status.MPI_SOURCE;
+		if (BROADCAST)
+		{
+			MPI_Bcast(metadata, 2, MPI_INT, 0, MPI_COMM_WORLD);
+		}
+		else
+		{
+			MPI_Recv(metadata, 2, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+		}
+		
 		int size_C = metadata[0] + metadata[1] - 1;
-		//		printf("Slave #%d received metadata: %d, %d", pid, metadata[0], metadata[1]);
 
-				// receive polynomials
+		// receive polynomials
 		long *P = new long[metadata[0]], *Q = new long[metadata[1]];
-		MPI_Recv(P, metadata[0], MPI_LONG, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
-		MPI_Recv(Q, metadata[1], MPI_LONG, MPI_ANY_SOURCE, 2, MPI_COMM_WORLD, &status);
-
+		if (BROADCAST)
+		{
+			MPI_Bcast(P, metadata[0], MPI_LONG, 0, MPI_COMM_WORLD);
+			MPI_Bcast(Q, metadata[1], MPI_LONG, 0, MPI_COMM_WORLD);
+		}
+		else
+		{
+			MPI_Recv(P, metadata[0], MPI_LONG, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
+			MPI_Recv(Q, metadata[1], MPI_LONG, MPI_ANY_SOURCE, 2, MPI_COMM_WORLD, &status);
+		}
+		
 		long *Res = karatsuba_worker(pid, P, metadata[0], Q, metadata[1], np);
-		//		print_polynomial("Res" + pid, Res, metadata[0] + metadata[1]);
+		//print_polynomial("Res" + pid, Res, metadata[0] + metadata[1]);
 
-				// send back result
-		MPI_Ssend(Res, size_C, MPI_LONG, sender, 3, MPI_COMM_WORLD);
+		// send back result
+		MPI_Ssend(Res, size_C, MPI_LONG, 0, 3, MPI_COMM_WORLD);
 	}
 
 	// cleans up all MPI state before exit of process 
